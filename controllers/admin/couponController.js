dfc16e481cb0c03dca33caf8fd80881e2bb3c4ae
ANJALYKRISHNA05 <anjalykrishna0503@gmail.com>
2025-06-05 20:45:12 +0530

    till order

--- controllers/admin/couponController.js
+++ controllers/admin/couponController.js
@@ -3,7 +3,7 @@ const Coupon = require("../../models/couponSchema");
 const getCoupons = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 6;
+    const limit = parseInt(req.query.limit) || 3;
     const skip = (page - 1) * limit;
 
     const totalCoupons = await Coupon.countDocuments();
