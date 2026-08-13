a3eaf1350c7a5455c01c823bacca95e0713a4bfa
ANJALYKRISHNA05 <anjalykrishna0503@gmail.com>
2026-08-13 17:54:01 +0530

    nodemailer changes

--- controllers/user/subscriptionController.js
+++ controllers/user/subscriptionController.js
@@ -2,7 +2,9 @@ const Subscription = require('../../models/subscriptionSchema');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.NODEMAILER_EMAIL,
         pass: process.env.NODEMAILER_PASSWORD
